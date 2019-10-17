5432ec9b48196218297d73ae3b29d88d22578e50
stephenjude <stephenjudesuccess@gmail.com>
2019-10-17 13:24:52 +0100

    add my twitter handle

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -65,7 +65,7 @@
 
         <ul class="list-reset inline-block text-center">
             <li class="inline-block">
-                <a href="https://twitter.com/username" target="_blank" title="Twitter">
+                <a href="https://twitter.com/stephenjudeso" target="_blank" title="Twitter">
                     <span class="fa-stack fa-lg">
                         <i class="fa fa-circle fa-stack-2x"></i>
                         <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
