97b70efe40cecc797e10e446339a620e68e0bff2
stephenjude <stephenjudesuccess@gmail.com>
2019-12-02 10:49:10 +0100

    removed on used curly brackets

--- resources/views/components/menu-responsive.blade.php
+++ resources/views/components/menu-responsive.blade.php
@@ -1,11 +1,14 @@
 <nav id="js-nav-menu" class="nav-menu hidden lg:hidden">
     <ul class="list-reset my-0">
         <li class="pl-4">
-            <a
-                title="{{ $meta['title'] ?? config('services.meta.site_name') }} About"
-                href="{{url('about')}}"
-                class="nav-menu__item hover:text-blue-500 active text-blue }}"
-            >About</a>
+            <a title="{{ $meta['title'] ?? config('services.meta.site_name') }} About" href="{{url('about')}}"
+                class="nav-menu__item hover:text-blue-500 active text-blue">About</a>
+        </li>
+        <li class="pl-4">
+            <a title="About {{ $meta['title'] ?? config('services.meta.site_name') }}" href="{{url('about')}}"
+                class="nav-menu__item hover:text-blue-500 active text-blue">
+                About
+            </a>
         </li>
     </ul>
 </nav>
