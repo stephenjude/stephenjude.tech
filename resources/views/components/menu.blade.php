d186a8ad4d9e45c5a893f6eeadcb37031b49cfa4
stephenjude <stephenjudesuccess@gmail.com>
2019-12-02 10:35:14 +0100

    add newsletter to menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,5 +1,11 @@
 <nav class="hidden lg:flex items-center justify-end text-lg">
-    <a title="{{ $meta['title'] ?? config('services.meta.site_name') }} About" href="{{url('about')}}" class="ml-6 text-gray-700 hover:text-blue-600 'active text-blue-600' }}">
+    <a title="newsletter" href="{{url('newsletter')}}"
+        class="ml-6 text-gray-700 hover:text-blue-600 'active text-blue-600' }}">
+        Newsletter
+    </a>
+    <a title="About {{ $meta['title'] ?? config('services.meta.site_name') }}" href="{{url('about')}}"
+        class="ml-6 text-gray-700 hover:text-blue-600 'active text-blue-600' }}">
         About
     </a>
+
 </nav>
