3b0e20cf611686eb5e4c80c470b3f5ed02673f09
stephenjude <stephenjudesuccess@gmail.com>
2019-12-02 10:54:00 +0100

    update newsletter nav item

--- resources/views/components/menu-responsive.blade.php
+++ resources/views/components/menu-responsive.blade.php
@@ -1,8 +1,8 @@
 <nav id="js-nav-menu" class="nav-menu hidden lg:hidden">
     <ul class="list-reset my-0">
         <li class="pl-4">
-            <a title="{{ $meta['title'] ?? config('services.meta.site_name') }} About" href="{{url('about')}}"
-                class="nav-menu__item hover:text-blue-500 active text-blue">About</a>
+            <a title="Newsletter" href="{{url('newsletter')}}"
+                class="nav-menu__item hover:text-blue-500 active text-blue">Newsletter</a>
         </li>
         <li class="pl-4">
             <a title="About {{ $meta['title'] ?? config('services.meta.site_name') }}" href="{{url('about')}}"
