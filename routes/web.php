0095218a3bae873d47cf02e905e26d6ea51d1957
stephenjude <stephenjudesuccess@gmail.com>
2019-12-16 16:58:58 +0100

    backup posts

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,17 @@
 |
 */
 
+use Wink\WinkPost;
+
 Route::get('/', 'BlogController@index')->name('home');
 Route::get('/articles', 'BlogController@index')->name('articles');
 Route::get('/about', 'BlogController@about')->name('about');
 Route::get('/articles/{slug}', 'BlogController@findPostBySlug')->name('blog.post');
 Route::get('/fresh', 'BlogController@updateIndexedArticles')->name('index.json');
 Route::get('/newsletter', 'BlogController@newsletter')->name('newsletter');
+
+Route::get('posts', function () {
+    $posts = WinkPost::with('tags')->get();
+
+    return response()->json($posts, 200);
+});
