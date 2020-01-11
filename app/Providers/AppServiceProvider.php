6f807716b0f5191dfa9512bd25b9119e0d712b8e
stephenjude <stephenjudesuccess@gmail.com>
2020-01-11 21:01:31 +0100

    backup posts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $post_publish_sql = "update `wink_posts` set `published` = ?, `wink_posts`.`updated_at` = ? where `id` = ?";
+
+        DB::listen(function ($query) use ($post_publish_sql) {
+            if ($post_publish_sql === $query->sql) {
+                Artisan::call('generate:feed');
+                Artisan::call('generate:index');
+            }
+        });
     }
 }
