2a4335441286972acbca7b850527dce1d1f16f87
Stephen Jude <StephenJudesuccess@gmail.com>
2020-05-10 22:10:44 +0100

    update newsletter

--- resources/views/components/newsletter-signup.blade.php
+++ resources/views/components/newsletter-signup.blade.php
@@ -1,29 +1,6 @@
 
 
-<div class="flex justify-center lg:-mx-12 my-12 p-6 md:px-12 bg-gray-200 border border-gray-400 text-sm md:rounded shadow">
-    <!-- Begin Mailchimp Signup Form -->
-    <div id="mc_embed_signup">
-        <form action="https://gmail.us20.list-manage.com/subscribe/post" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-            <div id="mc_embed_signup_scroll">
-                <h5 class="text-center">Join my weekly newsletter and never miss out on new tutorials, tips, and more.</h5>
-                <div class="mc-field-group">
-                    <label for="">Email Address</label>
-
-                    <input type="hidden" name="u" value="4e4ddd30dfc7de0c8a4bf6592">
-                    <input type="hidden" name="id" value="d3fe3975e4">
-                    <input type="email" name="MERGE0" class="required email" id="MERGE0" placeholder="Email address" required>
-                </div>
-                <div id="mce-responses" class="clear">
-                    <div class="response" id="mce-error-response" style="display:none"></div>
-                    <div class="response" id="mce-success-response" style="display:none"></div>
-                </div> <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-
-                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="" tabindex="-1" value=""></div>
-                <div class="clear">
-                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-            </div>
-        </form>
-    </div>
-    <!--End Mailchimp Signup Form -->
+<div  class="flex justify-center">
+    <script async data-uid="df9bb92aee" src="https://stephenjude.ck.page/df9bb92aee/index.js"></script>
 </div>
 
--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -8,15 +8,8 @@
 @endpush
 
 @section('newsletter')
-<section role="main" class="flex-auto w-full container max-w-4xl mx-auto px-6">
+<section role="main" class="flex-auto w-full container max-w-4xl mx-auto px-1">
     @include('components.newsletter-signup')
-    <h5 class="text-center" >
-        <a href="https://us20.campaign-archive.com/home/?u=4e4ddd30dfc7de0c8a4bf6592&id=d3fe3975e4">
-            Here is an archive of what I have sent out so far :)
-        </a>
-    </h5>
-
-    @include('components.ads')
 </section>
 
 @endsection
