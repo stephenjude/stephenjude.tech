34ba16438fd4731a5670c7039a02fbcf9c51422d
stephenjude <stephenjudesuccess@gmail.com>
2021-01-30 10:19:04 +0100

    update job banner

--- resources/views/components/job.blade.php
+++ resources/views/components/job.blade.php
@@ -4,12 +4,12 @@
             role="alert">
             <span class="flex rounded-full bg-indigo-700 uppercase px-2 py-1 text-xs font-bold mr-3">Hire me</span>
             <span class="font-semibold mr-2 text-left flex-auto">
-                Hi, I am currently looking for a remote PHP/Laravel developer role. Here is my <a
+                Hey, have you tried Litehost's fast & affordable web hosting packages? <a
                     class="text-indigo-100 hover:text-indigo-100 no-underline"
-                    href="https://drive.google.com/file/d/1_7pG3HFslrRTRa7d4ubrQyxdfFymcQCu/view?usp=sharing">resume</a>.
+                    href="https://getlitehost.com/sharedhosting.php">Click Here</a>.
             </span>
             <a class="text-indigo-100 "
-                href="https://drive.google.com/file/d/1_7pG3HFslrRTRa7d4ubrQyxdfFymcQCu/view?usp=sharing">
+                href="https://getlitehost.com/sharedhosting.php">
                 <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
                 </svg>
