f09e079f28ef4c997c773b6c33a6b325ecca0c05
stephenjude <stephenjudesuccess@gmail.com>
2021-01-30 10:25:55 +0100

    update ad copy

--- resources/views/components/job.blade.php
+++ resources/views/components/job.blade.php
@@ -6,10 +6,10 @@
             <span class="font-semibold mr-2 text-left flex-auto">
                 Hey, do you know you can deploy your Laravel application on a shared server? <a
                     class="text-indigo-100 hover:text-indigo-100 no-underline"
-                    href="https://getlitehost.com/sharedhosting.php">Click Here</a>.
+                    href="https://getlitehost.com/sharedhosting.php" target="_blank">Click Here</a>.
             </span>
             <a class="text-indigo-100 "
-                href="https://getlitehost.com/sharedhosting.php">
+                href="https://getlitehost.com/sharedhosting.php" target="_blank">
                 <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
                 </svg>
